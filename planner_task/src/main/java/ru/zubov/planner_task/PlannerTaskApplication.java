ece2663e39ae67153752a0fe2699dc6acbc29cb9
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-04-10 17:16:51 +0300

    Correct planner_task module

--- planner_task/src/main/java/ru/zubov/planner_task/PlannerTaskApplication.java
+++ planner_task/src/main/java/ru/zubov/planner_task/PlannerTaskApplication.java
@@ -2,10 +2,14 @@ package ru.zubov.planner_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = {"ru.zubov.planner_task"})
+@EntityScan(basePackages = {"ru.zubov"})
 public class PlannerTaskApplication {
 
     public static void main(String[] args) {
