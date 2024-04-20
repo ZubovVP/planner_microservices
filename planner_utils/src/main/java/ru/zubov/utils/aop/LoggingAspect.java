a168fd8980fe1b7f3a0aedbbe9ce557b6ddeee29
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-04-20 23:36:02 +0300

    Add planner utils

---
+++ planner_utils/src/main/java/ru/zubov/utils/aop/LoggingAspect.java
@@ -0,0 +1,42 @@
+package ru.zubov.utils.aop;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+@Log
+public class LoggingAspect {
+
+    //Аспект будет выполняться для всех методов контроллеров
+    @Around("execution(* ru.zubov.planner_task.task..*(..))")
+    public Object profileControllerMethods(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        MethodSignature methodSignature = (MethodSignature) proceedingJoinPoint.getSignature();
+
+        //Получить информацию о том, какой клаасс и метод выполняется
+        String className = methodSignature.getDeclaringType().getSimpleName();
+        String methodName = methodSignature.getName();
+
+        log.info("----------Executing " + className + "." + methodName + "    -------------------");
+
+        StopWatch stopWatch = new StopWatch();
+
+        // засекаем время
+        stopWatch.start();
+
+        // выполняем сам метод
+        Object proceed = proceedingJoinPoint.proceed();
+
+        // останавливаем таймер
+        stopWatch.stop();
+
+        log.info("----------Execution time of " + className + "." + methodName + " :: " + stopWatch.getTotalTimeMillis() + "ms    -------------------");
+
+        return proceed;
+    }
+}
