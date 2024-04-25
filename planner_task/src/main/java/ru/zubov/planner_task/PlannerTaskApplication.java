84806289a1af42ecd4035153b498ebffe545b393
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-04-26 00:07:11 +0300

    Try use component scan

--- planner_task/src/main/java/ru/zubov/planner_task/PlannerTaskApplication.java
+++ planner_task/src/main/java/ru/zubov/planner_task/PlannerTaskApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"ru.zubov.planner_task"})
 @EntityScan(basePackages = {"ru.zubov"})
+@ComponentScan(basePackages = {"ru.zubov"})
 public class PlannerTaskApplication {
 
     public static void main(String[] args) {
