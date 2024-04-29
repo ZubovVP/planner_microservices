3a621a0bdf070fa17f69218f0598e382e94db86c
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-04-30 00:24:47 +0300

    Add module planner_user

---
+++ planner_user/src/main/java/ru/zubov/planner_user/PlannerUserApplication.java
@@ -0,0 +1,13 @@
+package ru.zubov.planner_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlannerUserApplication.class, args);
+    }
+
+}
