85233de12b1ecaaa452ee4257ff3535eac92a304
Vitaly Zubov <Zubov.VP@yandex.ru>
2024-05-13 23:23:50 +0300

    Add WebFlux

--- planner_utils/src/main/java/ru/zubov/utils/restTemplate/UserRestBuilder.java
+++ planner_utils/src/main/java/ru/zubov/utils/restTemplate/UserRestBuilder.java
@@ -5,15 +5,27 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import ru.zubov.planner_entity.entity.User;
 
 @ConditionalOnProperty(name = "user.base.url")
 @Component
 public class UserRestBuilder {
     @Value("${user.base.url}")
     private String baseUrl;
-    
-    public boolean existUser(Long id){
-       return RestClient.create(baseUrl).get().uri("/id/{id}",  id).accept(MediaType.APPLICATION_JSON)
+
+    public boolean existUser(Long id) {
+        return RestClient.create(baseUrl).get().uri("/id/{id}", id).accept(MediaType.APPLICATION_JSON)
                 .exchange((request, response) -> response.getStatusCode().is2xxSuccessful());
+    }
+
+    public Flux<User> existUserAsync(Long id) {
+        return WebClient.create(baseUrl)
+                .get()
+                .uri("/id/{id}", id)
+                .retrieve()
+                .bodyToFlux(User.class);
+
     }
 }
